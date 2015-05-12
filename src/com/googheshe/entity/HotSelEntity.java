2be4ebc2aa4452a65ee1617470c36534759ae324
dpower2015 <dpower2015>
2015-05-13 01:45:50 +0800

    同步包缺失
    同步包缺失

---
+++ src/com/googheshe/entity/HotSelEntity.java
@@ -0,0 +1,21 @@
+package com.googheshe.entity;
+
+public class HotSelEntity {
+	private String android_img_note2_min_url;
+	private String android_img_note2_url;
+	private String android_img_note3_min_url;
+	private String android_img_note3_url;
+	private String goods_img_url;
+	private int    click_num;
+	private String companyName;
+	private String description;
+	private int id;
+	private String iphone56;
+	private String iphone56ImgUrl;
+	private String iphone56MinImagesUrl;
+	private String iphone6plusImages;
+	private int martPrice;
+	private int minprice;
+	private String name;
+	private int saleNum;
+}
