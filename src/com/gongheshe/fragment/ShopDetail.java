85d56ea358c69030ee4c14cedf8f4f4d52a0d6f5
dpower2015 <dpower2015>
2015-05-25 22:28:18 +0800

    登录修复
    登录修复

--- src/com/gongheshe/fragment/ShopDetail.java
+++ src/com/gongheshe/fragment/ShopDetail.java
@@ -21,7 +21,6 @@ import com.gongheshe.adapter.ShopDetailAdapter;
 import com.gongheshe.dialog.CityListPopWindow;
 import com.gongheshe.javabean.CityMod;
 import com.gongheshe.javabean.ProductMod;
-import com.gongheshe.model.TypeClassMod;
 import com.gongheshe.util.PullStagGridViewUT;
 import com.gongheshe.util.PullStagGridViewUT.OnStagGridViewListener;
 import com.gongheshe.util.ToastUtil;
@@ -93,7 +92,7 @@ public class ShopDetail extends BaseFragment implements View.OnClickListener {
 			public void onItemClick(int position) {
 				if(position > 0){
 					thirdlFragment.setTypeClassMod(adapter.datas.get(position-1));
-					baseActivity.replaceFragment(thirdlFragment, true);
+					baseActivity.replaceFragment(thirdlFragment, false);
 				}
 				
 				// ToastUtil.showToast(getActivity(), "点击事件" + position);
